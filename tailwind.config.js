e3732eb54b0e7319ab0802180232312b3c06c28d
Shivansh Vikram Singh <shivanshvikramsingh764@gmail.com>
2024-10-27 00:16:41 +0530

    updated features

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Specify where Tailwind should scan for class names
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
