4f9afd2d87f56d1c69eb75669e3d525c4a060101
Shivansh Vikram Singh <shivanshvikramsingh764@gmail.com>
2024-10-27 00:09:35 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Features from './components/Features';
+import NeuraxisClone from './Neuraxis-clone';
 import './App.css';
+import './tailwind.css';
+import AuthPage from './components/auth';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NeuraxisClone />}></Route>
+        <Route path="/auth" element={<AuthPage />}></Route>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
